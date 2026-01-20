c7f0b45eae4116772df92167ca5e334c487b8a1f
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-01-20 21:56:58 +0800

    feat(watcher): implement sales fetcher and slack notification

--- packages/watcher/src/index.ts
+++ packages/watcher/src/index.ts
@@ -1,5 +1,6 @@
 import { getSale } from '@weverse-shop/core'
 import { Hono } from 'hono'
+import { slack } from './slack'
 
 const app = new Hono()
 
@@ -7,19 +8,27 @@ app.get('/', (c) => {
   return c.text('Hello from @weverse-shop/watcher')
 })
 
-const saleIds = [43782, 51619]
-
 export default {
   fetch: app.fetch,
   // See wrangler.jsonc for information on cron triggers.
   // Currently, it's set to run every 15 minutes.
-  scheduled: async (controller: ScheduledController, env: Env, ctx: ExecutionContext) => {
+  scheduled: async (
+    controller: ScheduledController,
+    env: Cloudflare.Env,
+    ctx: ExecutionContext,
+  ) => {
     const task = async () => {
-      const sales = await getSale(43782, {
-        currency: 'KRW',
-        locale: 'zh-tw',
+      const saleIds = [43782, 51619]
+
+      const salesResult = saleIds.map(async (id) => {
+        const sales = await getSale(id, {
+          currency: 'KRW',
+          locale: 'zh-tw',
+        })
+        await slack({ text: `商品通知：${sales.name} ${sales.status}` })
       })
-      console.dir(sales, {depth: Infinity})
+
+      await Promise.all(salesResult)
     }
     ctx.waitUntil(task())
   },
---
+++ packages/watcher/src/slack.ts
@@ -0,0 +1,16 @@
+import { env } from "cloudflare:workers";
+
+export async function slack({text}: {text: string}) {
+  const response = await fetch(env.SLACK_WEBHOOK_URL, {
+    method: 'POST',
+    body: JSON.stringify({
+      text
+    })
+  })
+
+  if (!response.ok) {
+    throw Error('Slack Error')
+  }
+
+  return true
+}
