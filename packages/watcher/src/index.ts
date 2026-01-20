790d9013275ffaca542f7dd8452bcbd6cab37c18
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-01-20 21:25:27 +0800

    feat(watcher): implement cron handler

--- packages/watcher/src/index.ts
+++ packages/watcher/src/index.ts
@@ -1,9 +1,26 @@
+import { getSale } from '@weverse-shop/core'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello from @weverse-shop/watcher')
 })
 
-export default app
+const saleIds = [43782, 51619]
+
+export default {
+  fetch: app.fetch,
+  // See wrangler.jsonc for information on cron triggers.
+  // Currently, it's set to run every 15 minutes.
+  scheduled: async (controller: ScheduledController, env: Env, ctx: ExecutionContext) => {
+    const task = async () => {
+      const sales = await getSale(43782, {
+        currency: 'KRW',
+        locale: 'zh-tw',
+      })
+      console.dir(sales, {depth: Infinity})
+    }
+    ctx.waitUntil(task())
+  },
+}
