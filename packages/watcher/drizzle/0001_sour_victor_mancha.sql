4e5c97513aaa753ae1a490ceef8a29e8674a2dfc
Ethan Huang <69299174+chingweih@users.noreply.github.com>
2026-01-22 14:18:43 +0800

    chore(watcher): update schema

---
+++ packages/watcher/drizzle/0001_sour_victor_mancha.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_products` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`type` text DEFAULT 'weverse',
+	`sale_id` text NOT NULL,
+	`last_checked_at` integer,
+	`created_at` integer DEFAULT 1769062678888
+);
+--> statement-breakpoint
+INSERT INTO `__new_products`("id", "type", "sale_id", "last_checked_at", "created_at") SELECT "id", "type", "sale_id", "last_checked_at", "created_at" FROM `products`;--> statement-breakpoint
+DROP TABLE `products`;--> statement-breakpoint
+ALTER TABLE `__new_products` RENAME TO `products`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;
